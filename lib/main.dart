14aeec0838c0f284fee0121c4a14749f5d4a60ed
Do Nhat <dmnhat.pt@gmail.com>
2024-01-18 16:56:18 +0700

    Restructuring files and add CategoryGridItem widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories_screen.dart';
+import 'package:meals/screens/categories_screen.dart';
 
 final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
