4014c72c98179f8c3beb81af386070aad5ab7df0
Do Nhat <dmnhat.pt@gmail.com>
2024-01-22 09:58:40 +0700

    Playing the animation with AnimatedBuilder.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -23,13 +23,16 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
   @override
   void initState() {
+    super.initState();
+
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
       lowerBound: 0,
       upperBound: 1,
     );
-    super.initState();
+
+    _animationController.forward();
   }
 
   void _selectCategory(BuildContext context, Category category) {
@@ -49,23 +52,30 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
+      builder: (context, child) => Padding(
+        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+        child: child,
+      ),
     );
   }
 
